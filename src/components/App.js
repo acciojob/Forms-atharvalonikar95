05936e0a10ed376a49a4178cb598d7c7f3bf1dbf
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-05-21 02:02:15 +0530

    Implement routing with React Router in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,38 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Form from './Form';
+import FormRef from './FormRef';
+import FormState from './FormState';
 
+function App() {
+  return (
+    <BrowserRouter>
+
+      <div>
+        <Link id="form-link" to="/">
+          Form
+        </Link>
+
+        <br />
+
+        <Link id="form-ref-link" to="/form-ref">
+          Form Ref
+        </Link>
+
+        <br />
+
+        <Link id="form-state-link" to="/form-state">
+          Form State
+        </Link>
+      </div>
+      <Routes>
+        <Route path='/' element={<Form/>}></Route>
+        <Route path='/form-ref' element={<FormRef/>}></Route>
+        <Route path='/form-state' element={<FormState/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
